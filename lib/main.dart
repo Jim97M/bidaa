6dde41989f954d1618e54438af8627fa992f262c
wabuyajames@gmail.com <wabuyajames@gmail.com>
2021-11-20 22:49:05 +0300

    Authentication

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,8 @@
 import 'package:auth_fire/screens/login_screen.dart';
 import 'package:flutter/material.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
   runApp(const MyApp());
 }
 
---
+++ lib/models/user_model.dart
@@ -0,0 +1,10 @@
+class User {
+  String? uid;
+  String? email;
+  String? first_name;
+  String? second_name;
+  String? password;
+
+  UserModel(
+      {this.uid, this.email, this.first_name, this.second_name, this.password});
+}
